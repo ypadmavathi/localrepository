19a989249ae7b948f1cf862710d368437c1dc763
padmavathi.y <padmavathi.y@DS-9121AA344592.HCLT.CORP.HCL.IN>
2017-08-29 10:50:26 +0530

    commited.

---
+++ Repositoryproject/src/com/demo/Repositoryclass.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class Repositoryclass {
+
+	public static void main(String[] args) {
+		displayMethod();
+   
+   }
+
+	private static void displayMethod() {
+		System.out.println("HELLO");
+		
+	}
+
+}
